3ac17d09c216ea810ccffbfdc88e32cb3bdffd8e
vigneshgowrabathuni <vgowrabathuni@gmail.com>
2021-03-16 21:40:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto">
+        <h1 className="text-lg text-center p-10 font-bold">React JS and Tailwind CSS boilerplate</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
